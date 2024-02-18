a7a64ceb2fea016fc518efa43fe6a988712825c8
Russell Johnson <russ@johnsonville.net>
2024-02-17 23:04:40 -0600

    Implemented astroid but it broken does not do the same as the video.  Needs investigation.

---
+++ Assets/Scripts/Astroid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.0f;
+    [SerializeField]
+    private GameObject _explosionPreFab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.tag == "Laser")
+        {
+            if (transform.parent != null)
+            {
+                Instantiate(_explosionPreFab, transform.position, Quaternion.identity);
+                Destroy(other.gameObject);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
