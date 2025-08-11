04fee22666e1b1d095623d7373ccc584f46fbb17
Russell Johnson <russ@johnsonville.net>
2025-08-10 22:23:39 -0500

    Pause functionality done, but resume and main menu do not work.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,15 +9,25 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private bool isGameOver;
     public bool isCoopMode = false;
+    private bool _isPaused = false;
+    [SerializeField]
+    private GameObject _pauseMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            _isPaused = true;
+            Time.timeScale = 0;
+            _pauseMenu.SetActive(true);
+        }
+
         if (Input.GetKeyDown(KeyCode.R) && isGameOver == true)
         {
             SceneManager.LoadScene(0);
